05dab86f503b88715731c15d6fd686205edbeefb
fanite <fanite@qq.com>
2021-08-15 01:42:37 +0800

    overwrite find_element_by_image

--- autotest/webdriver.py
+++ autotest/webdriver.py
@@ -3,9 +3,36 @@ import time
 import aircv
 import logging
 import numpy as np
-from typing import Optional, Union
 from appium import webdriver
+from os.path import basename
+from collections import ChainMap
+from typing import Optional, Union
 from appium.webdriver.common.mobileby import MobileBy
+
+
+def find_all(im_source, im_search, **kwargs):
+    '''
+    优先Template，之后Sift
+    @ return [(x,y), ...]
+    '''
+    kwargs = ChainMap(kwargs, {"threshold": 0.8, "maxcnt": 0})
+    result = aircv.find_all_template(im_source, im_search, **kwargs)
+    if not result:
+        result = aircv.find_all_sift(im_source,
+                                     im_search,
+                                     maxcnt=kwargs["maxcnt"])
+    if not result:
+        return []
+    return [match["result"] for match in result]
+
+
+def find(im_source, im_search, **kwargs):
+    '''
+    Only find maximum one object
+    '''
+    kwargs = ChainMap(kwargs, {"maxcnt": 1})
+    r = find_all(im_source, im_search, **kwargs)
+    return r[0] if r else None
 
 
 class By(MobileBy):
@@ -16,9 +43,19 @@ class WebElement(webdriver.WebElement):
     pass
 
 
+class CoordElement(object):
+    def __init__(self, driver: "Remote", coord: "tuple") -> None:
+        super().__init__()
+        self.driver = driver
+        self.coord = coord
+
+    def click(self):
+        self.driver.tap([self.coord])
+
+
 class Remote(webdriver.Remote):
     def sleep(self, secs: Union["int", "float"]):
-        logging.info(f"等待 {secs} 秒...")
+        logging.info(f"waiting {secs} seconds...")
         time.sleep(secs)
 
     def save_screenshot(self,
@@ -32,18 +69,21 @@ class Remote(webdriver.Remote):
             cv2.IMREAD_COLOR)
         return cv2.imwrite(filename, image[y1:y2, x1:x2])
 
-    def find_element_by_image(self,
-                              img_path: str,
-                              cvt: "bool" = False) -> 'WebElement':
+    def find_element_by_image(
+            self,
+            img_path: str,
+            rgb: "bool" = False) -> Union["WebElement", "CoordElement"]:
+        logging.info(f"find picture {basename(img_path)} in screen.")
         try:
             return super().find_element_by_image(img_path)
         except:
-            target = cv2.imread(img_path)
-            template = cv2.imdecode(
+            im_search = cv2.imread(img_path)
+            im_source = cv2.imdecode(
                 np.frombuffer(self.get_screenshot_as_png(), np.uint8),
                 cv2.IMREAD_COLOR)
-            if cvt:
-                target = cv2.cvtColor(target)
-                template = cv2.cvtColor(template)
-            coor = aircv.find(target, template)
-            print(coor)
+            coord = find(im_source, im_search, rgb=rgb)
+            if not coord:
+                raise RuntimeError(
+                    f"the picture {basename(img_path)} is not found in screen."
+                )
+            return CoordElement(self, coord)
--- tests/test_example.py
+++ tests/test_example.py
@@ -11,14 +11,17 @@ T = TypeVar("T", bound=Union[webdriver.Remote,Custom])
 
 img_path = os.path.join(os.getcwd(), "asserts/test.png")
 
-def test_one(caplog):
-    log.info("asdsaddd")
+# def test_one(caplog):
+#     log.info("asdsaddd")
 
-def test_two(device: "T"):
-    device.activate_app("com.android.settings")
+# def test_two(device: "T"):
+#     pass
 
 def test_three(device: "T"):
-    device.save_screenshot(img_path, (0,886,1080,1086))
+    device.activate_app("com.android.settings")
+    device.sleep(3)
+    # device.save_screenshot(img_path, (0,886,1080,1086))
+    device.find_element_by_image(img_path, rgb=True).click()
 
 
 if __name__ == "__main__":
